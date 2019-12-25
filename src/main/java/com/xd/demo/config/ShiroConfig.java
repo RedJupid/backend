5700eda0337c6bf227d6d97bea17501f1bff5aaa
xd <627393061@qq.com>
2019-12-25 21:38:57 +0800

    登录

---
+++ src/main/java/com/xd/demo/config/ShiroConfig.java
@@ -0,0 +1,31 @@
+package com.xd.demo.config;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager){
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        // 设置 securityManager
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public SecurityManager securityManager(){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(userRealm());
+        return securityManager;
+    }
+
+    @Bean
+    public UserRealm userRealm(){
+        return new UserRealm();
+    }
+}
--- src/main/java/com/xd/demo/modules/sys/controller/UserController.java
+++ src/main/java/com/xd/demo/modules/sys/controller/UserController.java
@@ -3,9 +3,18 @@ package com.xd.demo.modules.sys.controller;
 
 import com.xd.demo.common.controller.BaseController;
 import com.xd.demo.modules.sys.entity.User;
+import io.swagger.annotations.Api;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * <p>
@@ -17,6 +26,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/sys/user")
+@Api(value = "用户控制器",tags={"用户操作接口"})
 public class UserController extends BaseController<User> {
 
+    @PostMapping("/login")
+    public String login(String username, String password){
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        try {
+            subject.login(token);
+            return "success";
+        }catch (UnknownAccountException e){
+            return "用户名不存在";
+        }catch (IncorrectCredentialsException e){
+            return "密码错误";
+        }
+
+    }
 }
