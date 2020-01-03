ea843f40193f6edeb0454f4141b8d70e0efe4078
xd <627393061@qq.com>
2020-01-03 14:16:17 +0800

    用户管理

--- src/main/java/com/xd/demo/modules/sys/controller/UserController.java
+++ src/main/java/com/xd/demo/modules/sys/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.xd.demo.modules.sys.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xd.demo.common.controller.BaseController;
 import com.xd.demo.common.util.Result;
 import com.xd.demo.modules.sys.entity.User;
 import com.xd.demo.modules.sys.service.IUserService;
 import io.swagger.annotations.Api;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +44,19 @@ public class UserController extends BaseController<User> {
     public Result getByUsername(String username){
         User user = userService.selectByUsername(username);
         return Result.success(user);
+    }
+
+    @GetMapping("/getByPage")
+    public Result getByPage(Integer current, Integer size, String username){
+        QueryWrapper<User> qw = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)){
+            qw.like("username","%"+username+"%");
+        }
+        Page<User> users = new Page<>();
+        users.setCurrent(current);
+        users.setSize(size);
+        IPage<User> page = userService.page(users, qw);
+        return Result.success(page);
     }
 
 }
